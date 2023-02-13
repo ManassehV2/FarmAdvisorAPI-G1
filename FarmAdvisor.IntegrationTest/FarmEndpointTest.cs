2faef39225a27fd6d1e64f57851b68856ed3455c
Dagim-Fikru <95189710+Dagim-Fikru@users.noreply.github.com>
2023-02-13 23:52:37 +0300

    integration test

---
+++ FarmAdvisor.IntegrationTest/FarmEndpointTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using FarmAdvisor.Models.Models;
+using Newtonsoft.Json;
+
+namespace FarmAdvisor_HttpFunctions.Tests
+{
+    public class FieldEndpointIntegrationTests
+    {
+        private readonly HttpClient _client;
+
+        public FieldEndpointIntegrationTests()
+        {
+            _client = new HttpClient();
+        }
+
+        [Fact]
+        public async Task AddFieldModel_ShouldReturnOkResult()
+        {
+            // Arrange
+            var url = "https://localhost:7071/api/AddFieldEndpoint";
+            var farmId = Guid.NewGuid();
+            var field = new FieldModel
+            {
+                FarmId = farmId,
+                Name = "Field 1",
+                Alt = 1000,
+                Polygon = "POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))"
+            };
+
+            // Act
+            var response = await _client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(field), Encoding.UTF8, "application/json"));
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<FieldModel>(content);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(field.Name, result.Name);
+            Assert.Equal(field.Alt, result.Alt);
+            Assert.Equal(field.Polygon, result.Polygon);
+        }
+
+        [Fact]
+        public async Task GetFieldModel_ShouldReturnOkResult()
+        {
+            // Arrange
+            var url = "https://localhost:7071/api/FieldApi/{id}";
+            var id = Guid.NewGuid();
+
+            // Act
+            var response = await _client.GetAsync(string.Format(url, id));
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<FieldModel>(content);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(id, result.FieldId);
+        }
+    }
+}
---
+++ FarmAdvisor.IntegrationTest/FarmFieldFunctionsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FarmAdvisor.HttpFunctions.Tests.Functions
+{
+    public class FarmFieldFunctionsTests
+    {
+        private readonly HttpClient _client;
+
+        public FarmFieldFunctionsTests()
+        {
+            _client = new HttpClient();
+        }
+
+        [Fact]
+        public async Task GetAllFarmFields_Should_Return_OK_With_Valid_FarmId()
+        {
+            // Arrange
+            string farmId = Guid.NewGuid().ToString();
+            string requestUrl = $"http://localhost:7071/api/users/farms/fields?farmId={farmId}";
+
+            // Act
+            var response = await _client.GetAsync(requestUrl);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAllFarmFields_Should_Return_BadRequest_With_Invalid_FarmId()
+        {
+            // Arrange
+            string farmId = "invalid";
+            string requestUrl = $"http://localhost:7071/api/users/farms/fields?farmId={farmId}";
+
+            // Act
+            var response = await _client.GetAsync(requestUrl);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
